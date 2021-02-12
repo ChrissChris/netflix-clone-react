1757b6eb01abb2289bcbf7da20ad2f7c8db20161
Chriss Chris <zenskech@outlook.com>
2021-02-12 18:37:49 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Row from './components/Row'
+import request from './request'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Row title='Netflix Originals' fetchURL={request.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchURL={request.fetchNetflixOriginals} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+//af33e7e6a89ca2db62170ccc337eed2d
---
+++ src/components/Row.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    effect
+    return () => {
+      cleanup
+    }
+  }, [input])
+
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+export default Row
