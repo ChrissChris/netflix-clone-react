ad93f6027e0f9465b6e41df70a901092dd1e37dd
Chriss Chris <zenskech@outlook.com>
2021-02-13 19:22:36 +0500

    edit

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,18 +1,34 @@
 import React, { useState, useEffect } from 'react'
+import axios from '../axios.js'
+
+const base_url = 'https://image.tmdb.org/t/p/original/'
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
-    effect
-    return () => {
-      cleanup
+    async function fetchData() {
+      try {
+        const request = await axios.get(fetchURL)
+        setMovies(...movies, request.data.results)
+        return request
+      } catch (error) {
+        console.log(error)
+      }
     }
-  }, [input])
+    fetchData()
+  }, [fetchURL])
 
+  console.log(movies)
   return (
-    <div>
+    <div className='row'>
       <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie)=>{
+          <img src="movie." alt=""/>
+        })}
+
+      </div>
     </div>
   )
 }
