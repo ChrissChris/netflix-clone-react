56a491d1e56786077224ac13fb4c2ca0c7515c80
Chriss Chris <zenskech@outlook.com>
2021-02-16 18:22:27 +0500

    edit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,44 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../axios.js'
+import request from '../request.js'
 import requests from '../request.js'
+import './Banner.css'
+
+// The url adress which will use to fetch the data from the API
+const apiPath = 'https://image.tmdb.org/t/p/original/'
+
+//We set a random number which will be passed as a index to access movie for the Banner
+const randomNum = Math.floor(Math.random() * 10)
 
 const Banner = () => {
   const [movie, setMovie] = useState([])
   useEffect(() => {
-    try {
-      async function fetchData() {
+    async function fetchData() {
+      try {
         const request = await axios.get(requests.fetchNetflixOriginals)
-        setMovie(
-          ...movie,
-          request.data.results[
-            Math.floor(Math.random() * request.data.results.lenth - 1)
-          ]
-        )
+        setMovie(request.data.results[randomNum])
         return request
+      } catch (error) {
+        console.log(error)
       }
-      fetchData()
-    } catch (error) {
-      console.log(error)
     }
-  }, [])
-  console.log(movie)
+    fetchData()
+  }, [request])
+
+  const truncateText = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str
+  }
+
   return (
     <header
       className='banner'
       style={{
         backgroundSize: 'cover',
         backgroundImage: `url(
-            'https://image.tmdb.org/t/p/original/${movie?.backdrop_path}'
+            "${apiPath}${movie?.backdrop_path}"
           )
           `,
         backgroundPosition: 'center center',
       }}>
-      <div className='banner_content'>
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-        <div className='banner_buttons'>
+      <div className='banner__content'>
+        <h1 className='banner__title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className='banner__buttons'>
           <button className='banner_button'>Play</button>
           <button className='banner_button'>My List</button>
         </div>
+        <h1 className='banner__description'>
+          {truncateText(movie?.overview, 150)}
+        </h1>
         {/* title */}
         {/* div > two buttons */}
       </div>
